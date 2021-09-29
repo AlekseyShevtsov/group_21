8feec63ca4791329fc41870767db85a9104f30f3
AlekseyShevtsov <jupitel@mail.ru>
2021-09-29 15:07:13 +0300

    add first jmeter hw

--- first_hw_jmeter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd work
-mkdir File1 File2 File3
-cd File2
-touch Eddy.txt Robby.txt Bob.txt version1.json version2.json
-mkdir Tests Documents Logs
-ls -la
-mv Bob.txt version1.json /d/work/File1
