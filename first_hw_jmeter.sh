3b91abe28a03cd126f8b393d2e826c68acdc1779
AlekseyShevtsov <jupitel@mail.ru>
2021-09-29 14:59:50 +0300

    test

---
+++ first_hw_jmeter.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd work
+mkdir File1 File2 File3
+cd File2
+touch Eddy.txt Robby.txt Bob.txt version1.json version2.json
+mkdir Tests Documents Logs
+ls -la
+mv Bob.txt version1.json /d/work/File1
